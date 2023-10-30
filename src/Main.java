bfec0a75bf9d5005c1aa54a44cf5d037f05b468d
TugasLatihan3 <akhmadzayni07@gmail.com>
2023-10-30 08:34:57 +0700

    menghitungluaslingkarang

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,21 @@ class HitungLuasLingkaran {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+/**inputan data luas lingkarang**
+ *
+ */
         System.out.print("Masukkan jari-jari lingkaran: ");
+
         double jariJari = input.nextDouble();
 
+/**menghitung luas lingkarang**
+ *
+ */
         double luas = Math.PI * Math.pow(jariJari, 2);
 
+/**outputan hasil luas lingkarang**
+ *
+ */
         System.out.println("Luas lingkaran dengan jari-jari " + jariJari + " adalah: " + luas);
     }
 }
